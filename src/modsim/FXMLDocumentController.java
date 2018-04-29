37c58229d9f8ce162708ce3a1aed5fafe49be5a9
exzone97 <exzone97@yahoo.co.id>
2018-04-29 16:07:14 +0700

    Memperbaiki semuanya

--- src/modsim/FXMLDocumentController.java
+++ src/modsim/FXMLDocumentController.java
@@ -6,30 +6,92 @@
 package modsim;
 
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author exzon
  */
 public class FXMLDocumentController implements Initializable {
-    
+
+    private Queue<String> q = new LinkedList<>();
+    private String temp = "";
+    private String que = "";
+
+    private int tempSaatIni = 0;
+
     @FXML
-    private Label label;
-    
+    private TextField tAntrean, tCS1, tCS2, tSaatIni, tTotalDiAntrean;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextArea tQueue;
+
+    @FXML
+    private void tambahButtonAction(ActionEvent event) {
+        if (!q.contains(tAntrean.getText()) && Integer.parseInt(tAntrean.getText()) > tempSaatIni) {
+            q.add((tAntrean.getText()));
+            if (tCS1.getText().equals("")) {
+                tCS1.setText(q.poll() + "");
+            } else if (tCS2.getText().equals("")) {
+                tCS2.setText(q.poll() + "");
+            } else {
+
+            }
+            this.getCurrentQueue();
+        }
+
     }
-    
+
+    @FXML
+    private void selesaiCS1Action(ActionEvent event) {
+        if (q.size() != 0) {
+            tCS1.setText(q.poll() + "");
+            this.getCurrentQueue();
+        }
+    }
+
+    @FXML
+    private void selesaiCS2Action(ActionEvent event) {
+        if (q.size() != 0) {
+            tCS2.setText(q.poll() + "");
+            this.getCurrentQueue();
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    public void getCurrentQueue() {
+        int x = q.size();
+
+        for (String s : q) {
+            temp = (s.toString());
+            que += temp + "\n";
+        }
+        System.out.println(que);
+        tQueue.setText(que);
+        que = "";
+        if (tCS1.getText().equals("")) {
+            tSaatIni.setText(tCS2.getText());
+            tTotalDiAntrean.setText(0 + "");
+            tempSaatIni = Integer.parseInt(tCS2.getText());
+        } else if (tCS2.getText().equals("")) {
+            tSaatIni.setText(tCS1.getText());
+            tTotalDiAntrean.setText(0 + "");
+            tempSaatIni = Integer.parseInt(tCS1.getText());
+        } else {
+            tSaatIni.setText(Math.max(Integer.parseInt(tCS1.getText()), Integer.parseInt(tCS2.getText())) + "");
+            tempSaatIni = (Math.max(Integer.parseInt(tCS1.getText()), Integer.parseInt(tCS2.getText())));
+            tTotalDiAntrean.setText(q.size() + "");
+        }
+    }
 }
